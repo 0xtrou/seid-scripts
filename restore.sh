6c289e3ee89e76c440a92dd3cdb79c034f1250f8
0xtrou <khangtran.btc@gmail.com>
2024-01-29 09:14:07 +0700

    chore: fixed backup scripts

--- restore.sh
+++ restore.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 # Restore previously backed up files
-cp ~/backups/node_key.json ~/.sei/config/node_key.json
+cp ~/backups/priv_validator_state.json ~/.sei/data/priv_validator_state.json
 cp ~/backups/priv_validator_key.json ~/.sei/config/priv_validator_key.json
 cp ~/backups/genesis.json ~/.sei/config/genesis.json
